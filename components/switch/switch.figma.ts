6dceb60a0b3a8b3b645499a23eebf12c2186acfa
Paul Martins <paul@opslevel.com>
2026-05-20 13:45:01 -0400

    feat: add Figma Code Connect for Switch

---
+++ components/switch/switch.figma.ts
@@ -0,0 +1,21 @@
+import figma, { html } from '@figma/code-connect/html'
+
+figma.connect('https://www.figma.com/design/o50GEVJJglLyQisSsOcd8q/Ant-Design-Vue-3?node-id=312-125131', {
+  props: {
+    checked: figma.enum('Type', {
+      checked: true,
+    }),
+    size: figma.enum('Size', {
+      default: undefined,
+      small: 'small',
+    }),
+    disabled: figma.enum('State', {
+      disabled: true,
+    }),
+    loading: figma.enum('State', {
+      loading: true,
+    }),
+  },
+  example: (props) =>
+    html`<a-switch${props.checked ? ' :checked="true"' : ''}${props.size ? ` size="${props.size}"` : ''}${props.disabled ? ' disabled' : ''}${props.loading ? ' loading' : ''} />`,
+})
